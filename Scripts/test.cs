a1e67eaffee2a4354e65026136686f00fa06efe8
RattyMyles <m.pucknell@gmail.com>
2020-11-01 22:48:29 +0000

    Update test.cs

--- Scripts/test.cs
+++ Scripts/test.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class test : MonoBehaviour
 {	
 	Rigidbody rb;
+	
+	// initiate game object of wintext
+	public GameObject winText;
 	
     // Start is called before the first frame update
     void Start()
@@ -27,13 +31,36 @@ public class test : MonoBehaviour
 			//Jump when you press space by adding force
 			//rb.AddForce(Vector3.up * 500);
 			
-			rb.velocity = Vector3.forward * 20f;
+			// it will go forward but be aware of the constraints in unity of the physics compononent
+			//rb.velocity = Vector3.forward * 20f;
+			
 		}
+		// Initiate new level or scene.
+		if (Input.GetKeyDown(KeyCode.R))
+			{
+				SceneManager.LoadScene("level2");	
+			}
     }
-	
+	// on click of a mouse, do action
 	private void OnMouseDown()
 	{
 		// On mouse click, it will delete the cube
 		//Destroy(gameObject,3f);
 	}
+	
+	private void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "Enemy")
+		{	
+			// If object collides with the game object, destroy it!
+			//Destroy(gameObject);
+			
+			// If object collides with game object, destroy object instead!
+			Destroy(collision.gameObject);
+			
+			winText.SetActive(true);
+		}
+	}
+	
 }
+
